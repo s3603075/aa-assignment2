bcafdae17818fc6ed56698843b7f5977c137ba8a
rmit-s3603075-danny-ho <s3603075@student.rmit.edu.au>
2017-09-23 13:43:26 +1000

    DFS not perfect

--- mazeGenerator/RecursiveBacktrackerGenerator.java
+++ mazeGenerator/RecursiveBacktrackerGenerator.java
@@ -1,6 +1,8 @@
 package mazeGenerator;
 
 import java.util.Collections;
+import java.util.Deque;
+import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -14,6 +16,9 @@ public class RecursiveBacktrackerGenerator implements MazeGenerator {
 	
 	//Array for set of directions (hex)
 	Integer dirSetX[] = {Maze.EAST, Maze.NORTHEAST,Maze.NORTHWEST,Maze.WEST,Maze.SOUTHWEST, Maze.SOUTHEAST};
+	
+	//Stack for backtracking
+	Deque<Cell> dfsStack = new ArrayDeque<Cell>();
 
 	@Override
 	public void generateMaze(Maze maze) {
@@ -27,39 +32,62 @@ public class RecursiveBacktrackerGenerator implements MazeGenerator {
 		Cell startCell = maze.map[randC][randR];
 		
 		/**IN PROGRESS - DFS for normal**/
-		//- Add to stack
-		//- Backtrack
-		
 		getUnvisited(startCell);
-		
-		/**END**/
 		
 	} // end of generateMaze()
 	
 	public Cell getUnvisited(Cell c)	{
-		Cell curCell;
+		
+		Cell neighCell = null;
 		
 		Collections.shuffle(Arrays.asList(dirSetN));
 		
 		for(int i = 0; i < dirSetN.length; i++)	{
 			//Find if neighbours are visited
-			if(c.neigh[dirSetN[i]].visited == false)	{
-				//Assign to current cell
-				curCell = c.neigh[dirSetN[i]];
-				//Set prev cell to visited
-				c.visited = true;
-				//Disable wall of start and current
-				c.wall[dirSetN[i]].present = false;
-				curCell.wall[Maze.oppoDir[dirSetN[i]]].present = false;
-				//Return current cell to function
-				return getUnvisited(curCell);
-			} else if (i == dirSetN.length - 1) {
-				System.out.println("Backtrack");
-				return null;
+			neighCell = c.neigh[dirSetN[i]]; 
+			if(neighCell != null) {
+				if(neighCell.visited == false)	{
+					//Set prev cell to visited, add to stack
+					c.visited = true;
+					dfsStack.push(c);
+					//Disable wall of start and current
+					c.wall[dirSetN[i]].present = false;
+					neighCell.wall[Maze.oppoDir[dirSetN[i]]].present = false;
+					//Return current cell to function
+					return getUnvisited(neighCell);
+				}
 			}
 		}
 		
+		if(dfsStack.peek() != null) {
+			return backtrack(dfsStack.pop());
+		}
+		
 		return null;
+		
+	}
+	
+	public Cell backtrack(Cell c) {
+		
+		Cell neighCell = null;
+
+		for(int i = 0; i < dirSetN.length; i++)	{
+			//Find if neighbours are visited
+			neighCell = c.neigh[dirSetN[i]];
+			if(neighCell != null) {
+				if(neighCell.visited == false)	{
+					return getUnvisited(neighCell);
+				}
+			}
+		}
+		
+		if(dfsStack.peek() != null) {
+			return backtrack(dfsStack.pop());
+		}
+		
+		return null;
+		
+		
 	}
 
 } // end of class RecursiveBacktrackerGenerator
